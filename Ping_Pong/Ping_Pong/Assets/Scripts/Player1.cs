8c55802d3d1ff2512f7aad9da24a0525f46c832f
Andre <Andre>
2017-12-07 21:23:46 +0100

    -Suitable image for the ball was found and added to the images folder -Script for the player2 was created under the scripts folder and linked to its respective game object. -The player2 script was completed which allows for it to be controlled via the mouse, without exceeding the background borders. -Script for the player was created under the scripts folder and linked to its respective game object. -Code was added to the player script such that the player can moved each time an up/down key is pressed. The movement however is still flawed, as holding an up/down key will move the player a small distance, rather than continuous movement

---
+++ Ping_Pong/Ping_Pong/Assets/Scripts/Player1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInUnits = (Input.mousePosition.y / Screen.width * 16) - 6;
+
+        Vector3 newPlayerPos = new Vector3(this.transform.position.x, mousePosInUnits,
+                                                this.transform.position.z);
+
+        newPlayerPos.y = Mathf.Clamp(mousePosInUnits, -4.33f, 4.33f);
+
+        this.transform.position = newPlayerPos;
+    }
+}
---
+++ Ping_Pong/Ping_Pong/Assets/Scripts/Player2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            if (position.y < 4.33f)
+            {
+                this.transform.position = position;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y = position.y - 1.5f;
+            if (position.y > -4.33f)
+            {
+                this.transform.position = position;
+            }
+        }
+    }
+}
+
