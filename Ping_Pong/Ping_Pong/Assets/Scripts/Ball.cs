41ca6a0d887ddf5b719a790c4a1bb03050eb5be1
Andre <Andre>
2017-12-10 16:56:06 +0100

    -Dragged the ball from the images folder into the level 1 scene. -The following components were added to the ball: Rigidbody 2D, Circle Collider 2D -Created a Physics Material 2D which was added to the ball under the CircleCollider 2D component. -Created a Script called 'Ball' under the script folder, this script was then linked to the 'ball' object in Level_01 -The necessary code in the script was created such that the ball will follow Player1 until the spacebar is pressed.

---
+++ Ping_Pong/Ping_Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    Player1 player;
+    Player2 player2;
+    Vector3 player2BallPosDiff;
+    bool gameStarted = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindObjectOfType<Player1>();
+        player2 = GameObject.FindObjectOfType<Player2>();
+        player2BallPosDiff = this.transform.position - player2.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!gameStarted)
+        {
+            this.transform.position = player2.transform.position + player2BallPosDiff;
+        }
+
+        if (Input.GetKeyDown("space") && !gameStarted)
+        {
+            gameStarted = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+        }
+
+    }
+}
