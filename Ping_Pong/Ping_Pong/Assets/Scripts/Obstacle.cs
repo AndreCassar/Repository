bc0ab91992f989c8f6ef8775632c35fe22b15b14
Andre <Andre>
2017-12-13 20:04:05 +0100

    -Dragged the objects from the prefabs folder into the scene Level_03. -Created and completed the script 'Obstacle' which controls the movement of the obstacles.

---
+++ Ping_Pong/Ping_Pong/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+    string dir = "down";
+    // Use this for initialization
+    void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = this.transform.position;
+        if (dir.Equals("down"))
+        {
+            if (position.y <= -4.33f)
+            {
+                dir = "up";
+
+            }
+            position.y = position.y - 0.05f;
+            this.transform.position = position;
+        }
+        if (dir.Equals("up"))
+        {
+            if (position.y >= 4.33f)
+            {
+                dir = "down";
+
+            }
+            position.y = position.y + 0.05f;
+            this.transform.position = position;
+        }
+    }
+}
